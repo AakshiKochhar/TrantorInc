d94c5caf9212473fd8c07e096f8ebd18079250d8
AakshiKochhar <58536123+AakshiKochhar@users.noreply.github.com>
2020-07-27 20:00:49 -0700

    Update StockData.py

--- StockData.py
+++ StockData.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[32]:
-
-
 """ The following program will use Pandas to plot and get statistics from stock data."""
 import datetime
 import pandas
@@ -21,10 +15,6 @@ end_date = datetime.datetime(2019, 12, 31)
 dataframe = pandas_datareader.data.DataReader("AMZN", 'yahoo', initial_date, end_date) # preparing to call for Amazon stock data
 dataframe.tail()
 
-
-# In[33]:
-
-
 # Calculates the 'roling average'.
 closing_price = dataframe['Adj Close']
 rolling_average = closing_price.rolling(window=40).mean()
@@ -41,34 +31,14 @@ closing_price.plot(label='Amazon')
 rolling_average.plot(label='Rolling average')
 matplotlib.pyplot.legend()
 
-
-# In[27]:
-
-
 # Compares IBM to its competitors.
 dataframe_competition = pandas_datareader.data.DataReader(['AAPL', 'GOOGL', 'IBM', 'MSFT', 'AMZN', 'ORCL'],'yahoo',start=initial_date,end=end_date)['Adj Close']
 dataframe_competition.tail() 
 
-
-# In[28]:
-
-
 returns_competition = dataframe_competition.pct_change()
-
 corr = returns_competition.corr()
-
-
-# In[29]:
-
 
 # Plots comparison of return distributions between companies.
 matplotlib.pyplot.scatter(returns_competition.AMZN, returns_competition.ORCL)
 matplotlib.pyplot.ylabel('Returns Oracle')
 matplotlib.pyplot.xlabel('Returns Amazon')
-
-
-# In[ ]:
-
-
-
-
