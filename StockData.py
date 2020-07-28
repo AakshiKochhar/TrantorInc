b5093a2667e9dbb2c976a6f09c81203591858aaa
AakshiKochhar <58536123+AakshiKochhar@users.noreply.github.com>
2020-07-27 19:59:58 -0700

    Add files via upload

--- StockData.py
+++ StockData.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
 """ The following program will use Pandas to plot and get statistics from stock data."""
 import datetime
 import pandas
@@ -12,8 +18,12 @@ initial_date = datetime.datetime(2010, 1, 1)
 end_date = datetime.datetime(2019, 12, 31)
 
 # Will collect IBM's stock data.
-dataframe = pandas_datareader.data.DataReader("IBM", 'yahoo', start, end) # preparing to call for IBM stock data
+dataframe = pandas_datareader.data.DataReader("AMZN", 'yahoo', initial_date, end_date) # preparing to call for Amazon stock data
 dataframe.tail()
+
+
+# In[33]:
+
 
 # Calculates the 'roling average'.
 closing_price = dataframe['Adj Close']
@@ -27,18 +37,38 @@ matplotlib.__version__
 
 # Creates the 'rolling average' graph.
 style.use('ggplot')
-closing_price.plot(label='IBM')
+closing_price.plot(label='Amazon')
 rolling_average.plot(label='Rolling average')
 matplotlib.pyplot.legend()
 
+
+# In[27]:
+
+
 # Compares IBM to its competitors.
-dataframe_competition = web.DataReader(['AAPL', 'GOOGL', 'IBM', 'MSFT', 'AMZN'],'yahoo',start=start,end=end)['Adj Close']
+dataframe_competition = pandas_datareader.data.DataReader(['AAPL', 'GOOGL', 'IBM', 'MSFT', 'AMZN', 'ORCL'],'yahoo',start=initial_date,end=end_date)['Adj Close']
 dataframe_competition.tail() 
 
+
+# In[28]:
+
+
 returns_competition = dataframe_competition.pct_change()
+
 corr = returns_competition.corr()
 
+
+# In[29]:
+
+
 # Plots comparison of return distributions between companies.
-matplotlib.pyplot.scatter(returns_competition.IBM, returns_competition.MSFT)
-matplotlib.pyplot.ylabel('Returns Microsoft')
-matplotlib.pyplot.xlabel('Returns IBM')
+matplotlib.pyplot.scatter(returns_competition.AMZN, returns_competition.ORCL)
+matplotlib.pyplot.ylabel('Returns Oracle')
+matplotlib.pyplot.xlabel('Returns Amazon')
+
+
+# In[ ]:
+
+
+
+
